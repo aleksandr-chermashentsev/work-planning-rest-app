7ff663ca649ae55d31f47ed96974d75a0a796cec
Aleksandr Chermashentsev <aleksandr.chermashentsev@karuna.group>
2022-03-05 12:21:59 +0700

    Initial commit

---
+++ src/main/java/com/achermashentsev/workplanningservice/WorkPlanningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.achermashentsev.workplanningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPlanningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkPlanningServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/achermashentsev/workplanningservice/WorkPlanningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achermashentsev.workplanningservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkPlanningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
